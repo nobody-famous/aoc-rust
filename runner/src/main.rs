3405f183f16915326f897ccc01969f971cf375ca
Rich Heller <rellehr@gmail.com>
2022-12-05 23:13:35 -0600

    Part 1

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,6 +3,7 @@ use std::time;
 use year2022::day1;
 use year2022::day2;
 use year2022::day3;
+use year2022::day4;
 
 fn main() {
     let problems: Vec<Problem> = vec![
@@ -14,7 +15,7 @@ fn main() {
         Problem::new("day3/Part2".to_string(), day3::part2::solve),
     ];
 
-    // let problems: Vec<Problem> = vec![Problem::new("day3/Part2".to_string(), day3::part2::solve)];
+    let problems: Vec<Problem> = vec![Problem::new("day4/Part1".to_string(), day4::part1::solve)];
 
     let total = run_all(problems) as f64 / 1e6;
 
