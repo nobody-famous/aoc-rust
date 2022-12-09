c79764882d278247c09243549da68312d1da329c
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2022-12-08 18:30:48 -0600

    Day7 (#4)
    * Part 1
    
    * Part 2

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -6,6 +6,7 @@ use year2022::day3;
 use year2022::day4;
 use year2022::day5;
 use year2022::day6;
+use year2022::day7;
 
 fn main() {
     let problems: Vec<Problem> = vec![
@@ -21,9 +22,11 @@ fn main() {
         Problem::new("day5/part2".to_string(), day5::part2::solve),
         Problem::new("day6/part1".to_string(), day6::part1::solve),
         Problem::new("day6/part2".to_string(), day6::part2::solve),
+        Problem::new("day7/part1".to_string(), day7::part1::solve),
+        Problem::new("day7/part2".to_string(), day7::part2::solve),
     ];
 
-    // let problems: Vec<Problem> = vec![Problem::new("day6/part2".to_string(), day6::part2::solve)];
+    // let problems: Vec<Problem> = vec![Problem::new("day7/part2".to_string(), day7::part2::solve)];
 
     let total = run_all(problems) as f64 / 1e6;
 
---
+++ year2022/src/day7/utils.rs
@@ -0,0 +1,61 @@
+pub const FILE_NAME: &str = "year2022/src/day7/puzzle.txt";
+
+#[derive(Debug)]
+pub struct State {
+    pub stack: Vec<usize>,
+    pub found: Vec<usize>,
+    pub root: usize,
+}
+
+impl State {
+    pub fn new() -> Self {
+        State {
+            stack: vec![0],
+            found: vec![],
+            root: 0,
+        }
+    }
+}
+
+pub fn do_work(lines: Vec<String>, pop: fn(&mut State) -> ()) -> State {
+    let mut state = State::new();
+
+    for line in lines {
+        if line.starts_with('$') {
+            cmd(&mut state, line, pop);
+        } else if !line.starts_with("dir") {
+            handle_file(&mut state, line);
+        }
+    }
+
+    while !state.stack.is_empty() {
+        pop(&mut state);
+    }
+
+    state
+}
+
+fn handle_file(state: &mut State, line: String) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    match parts.get(0) {
+        Some(s) => match s.parse::<usize>() {
+            Ok(n) => match state.stack.pop() {
+                Some(item) => state.stack.push(item + n),
+                None => todo!(),
+            },
+            Err(_) => todo!(),
+        },
+        None => todo!(),
+    }
+}
+
+fn cmd(state: &mut State, line: String, pop: fn(&mut State) -> ()) {
+    let parts: Vec<&str> = line.split(' ').collect();
+
+    match (parts.get(1), parts.get(2)) {
+        (Some(s1), Some(s2)) if *s1 == "cd" && *s2 != ".." => state.stack.push(0),
+        (Some(s1), Some(s2)) if *s1 == "cd" && *s2 == ".." => pop(state),
+        _ => (),
+    }
+}
