30bc77afadb4bb7fc9472a9645a92ef436f66cb0
Rich Heller <rellehr@gmail.com>
2022-12-09 11:12:42 -0600

    Part 2

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -26,9 +26,10 @@ fn main() {
         Problem::new("day7/part1".to_string(), day7::part1::solve),
         Problem::new("day7/part2".to_string(), day7::part2::solve),
         Problem::new("day8/part1".to_string(), day8::part1::solve),
+        Problem::new("day8/part2".to_string(), day8::part2::solve),
     ];
 
-    let problems: Vec<Problem> = vec![Problem::new("day8/part2".to_string(), day8::part2::solve)];
+    // let problems: Vec<Problem> = vec![Problem::new("day8/part2".to_string(), day8::part2::solve)];
 
     let total = run_all(problems) as f64 / 1e6;
 
--- year2022/src/day8/part2.rs
+++ year2022/src/day8/part2.rs
@@ -1,6 +1,6 @@
 use super::utils::{parse_rows, FILE_NAME};
 
-const CORRECT_ANSWER: usize = 0;
+const CORRECT_ANSWER: usize = 595080;
 
 pub fn solve() -> Result<(), String> {
     core::do_work(FILE_NAME, CORRECT_ANSWER, get_answer, |a, b| a == b)
@@ -8,25 +8,56 @@ pub fn solve() -> Result<(), String> {
 
 fn get_answer(lines: Vec<String>) -> usize {
     let rows = parse_rows(lines);
-    let mut scores: Vec<Vec<usize>> = rows
-        .clone()
-        .iter()
-        .map(|row| row.clone().iter().map(|_| 0).collect())
-        .collect();
+    let mut high: usize = 0;
 
     for row in 1..rows.len() - 1 {
         for col in 1..rows[0].len() - 1 {
-            println!("{},{}", row, col);
+            let score = get_score(&rows, row, col);
+            if score > high {
+                high = score;
+            }
         }
     }
 
-    println!("SCORES {:?}", scores);
-
-    0
+    high
 }
 
 fn get_score(rows: &Vec<Vec<usize>>, row: usize, col: usize) -> usize {
-    0
+    let target = rows[row][col];
+    let mut north = 0;
+    let mut south = 0;
+    let mut east = 0;
+    let mut west = 0;
+
+    for idx in (0..row).rev() {
+        north += 1;
+        if rows[idx][col] >= target {
+            break;
+        }
+    }
+
+    for idx in row + 1..rows.len() {
+        south += 1;
+        if rows[idx][col] >= target {
+            break;
+        }
+    }
+
+    for idx in (0..col).rev() {
+        west += 1;
+        if rows[row][idx] >= target {
+            break;
+        }
+    }
+
+    for idx in col + 1..rows[0].len() {
+        east += 1;
+        if rows[row][idx] >= target {
+            break;
+        }
+    }
+
+    north * south * east * west
 }
 
 #[cfg(test)]
