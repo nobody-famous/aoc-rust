693fa8ea2dea86c8b7793b6796a2b2f13287a79b
Rich Heller <rellehr@gmail.com>
2022-12-18 12:10:40 -0600

    Might be right, too slow

--- year2022/src/day16/part1.rs
+++ year2022/src/day16/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use crate::day16::utils::State;
 
@@ -13,14 +13,10 @@ pub fn solve() -> Result<(), String> {
 #[derive(Debug, PartialEq, Eq)]
 struct ToVisit {
     pub name: String,
-    pub seen: HashSet<String>,
+    pub remaining: HashSet<String>,
+    pub steps: usize,
     pub dist: usize,
-}
-
-impl ToVisit {
-    pub fn new(name: String, seen: HashSet<String>, dist: usize) -> Self {
-        ToVisit { name, seen, dist }
-    }
+    pub path: Vec<String>,
 }
 
 impl Ord for ToVisit {
@@ -35,36 +31,81 @@ impl PartialOrd for ToVisit {
     }
 }
 
+fn print_dists(dists: &HashMap<String, HashMap<String, usize>>) {
+    let mut dist_vec: Vec<(&String, &HashMap<String, usize>)> = dists.iter().map(|e| e).collect();
+
+    dist_vec.sort_by(|(n1, _), (n2, _)| n1.cmp(n2));
+
+    dist_vec.iter().for_each(|(name, dist)| {
+        let mut v: Vec<(&String, &usize)> = dist.iter().map(|e| e).collect();
+
+        v.sort_by(|(n1, _), (n2, _)| n1.cmp(n2));
+
+        print!("{:?}", name);
+        v.iter().for_each(|(_, s)| print!(" {:?}", s));
+        println!();
+    });
+}
+
 fn get_answer(lines: Vec<String>) -> usize {
     let valves = parse(lines);
     let dist_map = build_map(&valves);
+    let max_flow = valves.iter().fold(0, |acc, (_, valve)| acc + valve.rate) * 30;
     let state = State::new(valves, dist_map);
     let mut to_visit: BinaryHeap<ToVisit> = BinaryHeap::new();
+    let mut answer: Option<usize> = None;
 
-    to_visit.push(ToVisit::new(String::from("AA"), HashSet::new(), 0));
+    print_dists(&state.dist_map);
 
-    while let Some(node) = to_visit.pop() {
-        println!("NODE {:?}", node);
-        if let (Some(aa_map), Some(aa_valve)) =
-            (state.dist_map.get(&node.name), state.valves.get(&node.name))
-        {
-            println!("  {:?} {:?}", node.name, aa_valve);
+    to_visit.push(ToVisit {
+        name: String::from("AA"),
+        remaining: state.to_open.iter().fold(HashSet::new(), |mut acc, v| {
+            acc.insert(v.clone());
+            acc
+        }),
+        dist: 0,
+        steps: 0,
+        path: vec![],
+    });
 
-            state.to_open.iter().for_each(|name| {
-                if let (Some(dist), Some(valve)) = (aa_map.get(name), state.valves.get(name)) {
-                    println!(
-                        "    {:?}: {:?} * {:?} = {:?}",
-                        name,
-                        dist + 1,
-                        valve.rate,
-                        (dist + 1) * valve.rate
-                    );
-                }
-            });
+    while answer == None {
+        if let Some(node) = to_visit.pop() {
+            if node.remaining.len() == 0 {
+                answer = Some(max_flow - node.dist);
+            }
+
+            if let Some(dists) = state.dist_map.get(&node.name) {
+                node.remaining.iter().for_each(|name| {
+                    if let (Some(dist), Some(valve)) = (dists.get(name), state.valves.get(name)) {
+                        let new_steps = node.steps + dist + 1;
+                        let new_dist = (new_steps * valve.rate) + node.dist;
+                        let mut new_rem = node.remaining.clone();
+                        let mut new_path = node.path.clone();
+
+                        new_rem.remove(name);
+                        new_path.push(node.name.clone());
+
+                        to_visit.push(ToVisit {
+                            name: name.clone(),
+                            remaining: new_rem,
+                            steps: new_steps,
+                            dist: new_dist,
+                            path: new_path,
+                        })
+                    }
+                });
+            } else {
+                assert!(false);
+            }
+        } else {
+            assert!(false);
         }
     }
 
-    0
+    match answer {
+        Some(n) => n,
+        None => todo!(),
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +126,32 @@ mod tests {
             "Valve II has flow rate=0; tunnels lead to valves AA, JJ".to_string(),
             "Valve JJ has flow rate=21; tunnel leads to valve II".to_string(),
         ];
+
+        // 0 0 A
+        // 0 0 A,D
+
+        // A 0
+        // B 13
+        // C 2
+        // D 20
+        // E 3
+        // F 0
+        // G 0
+        // H 22
+        // I 0
+        // J 21
+
+        //   A B C D E F G H I J
+        // A 0 1 2 1 2 3 4 5 1 2
+        // B 1 0 1 2 3 4 5 6 2 3
+        // C 2 1 0 1 2 3 4 5 3 4
+        // D 1 2 1 0 1 2 3 4 2 3
+        // E 2 3 2 1 0 1 2 3 3 4
+        // F 3 4 3 2 1 0 1 2 4 5
+        // G 4 5 4 3 2 1 0 1 5 6
+        // H 5 6 5 4 3 2 1 0 6 7
+        // I 1 2 3 2 3 4 5 6 0 1
+        // J 2 3 4 3 4 5 6 7 1 0
 
         assert_eq!(get_answer(lines), 1651)
     }
