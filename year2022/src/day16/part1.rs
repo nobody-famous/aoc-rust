6af070c4c705fa699a2abe61d0c2be048783fa07
Rich Heller <rellehr@gmail.com>
2022-12-21 17:48:15 -0600

    Part 1 works, but slow

--- year2022/src/day16/part1.rs
+++ year2022/src/day16/part1.rs
@@ -2,32 +2,11 @@ use std::collections::{HashMap, HashSet};
 
 use super::utils::{build_map, parse, Valve, FILE_NAME};
 
-const CORRECT_ANSWER: usize = 0;
+const CORRECT_ANSWER: usize = 1716;
 
 pub fn solve() -> Result<(), String> {
     core::do_work(FILE_NAME, CORRECT_ANSWER, get_answer, |a, b| a == b)
 }
-
-// #[derive(Debug, PartialEq, Eq)]
-// struct ToVisit {
-//     pub name: String,
-//     pub remaining: HashSet<String>,
-//     pub steps: usize,
-//     pub dist: usize,
-//     pub path: Vec<String>,
-// }
-
-// impl Ord for ToVisit {
-//     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-//         other.dist.cmp(&self.dist)
-//     }
-// }
-
-// impl PartialOrd for ToVisit {
-//     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-//         other.dist.partial_cmp(&self.dist)
-//     }
-// }
 
 fn print_dists(dists: &HashMap<String, HashMap<String, usize>>) {
     let mut dist_vec: Vec<(&String, &HashMap<String, usize>)> = dists.iter().map(|e| e).collect();
@@ -51,6 +30,8 @@ struct Config {
     to_open: Vec<String>,
     to_mask: HashMap<String, usize>,
     from_mask: HashMap<usize, String>,
+    max_time: usize,
+    max_flow: usize,
 }
 
 #[derive(Debug)]
@@ -58,32 +39,28 @@ struct Node {
     cur: String,
     seen: HashSet<String>,
     time: usize,
+    flow: usize,
 }
-
-// struct TargetInfo {
-//     mask: usize,
-//     rate: usize,
-//     dist: usize,
-// }
-
-// type Cache = HashMap<Node, usize>;
 
 fn get_answer(lines: Vec<String>) -> usize {
     let valves = parse(lines);
     let dist_map = build_map(&valves);
-    let max_flow = valves.iter().fold(0, |acc, (_, valve)| acc + valve.rate) * 30;
     let to_open = valves
         .iter()
         .filter(|(_, v)| v.rate > 0)
         .map(|(name, _)| name.clone())
         .collect::<Vec<String>>();
     let (to_mask, from_mask) = assign_masks(&to_open);
+    let total_flow = valves.iter().fold(0, |acc, (_, valve)| acc + valve.rate);
+    let max_flow = total_flow * 30;
     let cfg = Config {
         valves,
         dist_map,
         to_open,
         to_mask,
         from_mask,
+        max_time: 30,
+        max_flow,
     };
 
     let dist = walk(
@@ -92,10 +69,18 @@ fn get_answer(lines: Vec<String>) -> usize {
             cur: String::from("AA"),
             seen: HashSet::new(),
             time: 0,
+            flow: 0,
         },
     );
 
-    max_flow - dist
+    dist
+}
+
+fn get_flow(cfg: &Config, name: &String) -> usize {
+    match cfg.valves.get(name) {
+        Some(valve) => valve.rate,
+        None => todo!(),
+    }
 }
 
 fn get_dist(
@@ -134,13 +119,16 @@ fn assign_masks(names: &Vec<String>) -> (HashMap<String, usize>, HashMap<usize, 
 
 fn walk(cfg: &Config, node: Node) -> usize {
     let mut to_visit: Vec<Node> = get_to_visit(cfg, &node);
+    let mut highest: usize = 0;
 
     while to_visit.len() > 0 {
-        // println!("TIMES: {:?}", to_visit.iter().map(|n| n.time).collect::<Vec<usize>>());
-
         let mut next_nodes: Vec<Node> = vec![];
 
         for item in to_visit {
+            if item.flow > highest {
+                highest = item.flow;
+            }
+
             let mut new_to_visit = get_to_visit(cfg, &item);
             next_nodes.append(&mut new_to_visit);
         }
@@ -148,12 +136,16 @@ fn walk(cfg: &Config, node: Node) -> usize {
         to_visit = next_nodes;
     }
 
+    // highest
     0
 }
 
 fn to_node(cfg: &Config, node: &Node, target: &String) -> Node {
     let mut new_seen = node.seen.clone();
     let dist = get_dist(&cfg.dist_map, &node.cur, target);
+    let flow = get_flow(cfg, target);
+    let rem_time = cfg.max_time - (node.time + dist + 1);
+    let new_flow = node.flow + (flow * rem_time);
 
     new_seen.insert(String::from(target));
 
@@ -161,6 +153,7 @@ fn to_node(cfg: &Config, node: &Node, target: &String) -> Node {
         cur: String::from(target),
         seen: new_seen,
         time: node.time + dist + 1,
+        flow: new_flow,
     }
 }
 
@@ -173,127 +166,6 @@ fn get_to_visit(cfg: &Config, node: &Node) -> Vec<Node> {
         .filter(|node| node.time < 30)
         .collect()
 }
-
-// fn new_walk(cfg: &Config, cache: &mut Cache, node: Node) -> usize {
-//     let to_visit = get_to_visit(cfg, node.seen);
-
-//     match cache.get(&node) {
-//         Some(v) => *v,
-//         _ => {
-//             let least = to_visit.iter().fold(usize::MAX, |acc, target| {
-//                 match cfg.dist_map.get(&node.name) {
-//                     Some(dists) => {
-//                         let to_end = process_kid(cfg, cache, &node, target, dists);
-
-//                         if to_end < acc {
-//                             to_end
-//                         } else {
-//                             acc
-//                         }
-//                     }
-//                     None => todo!(),
-//                 }
-//             });
-
-//             cache.insert(node.clone(), least);
-
-//             least
-//         }
-//     }
-// }
-
-// fn get_info(cfg: &Config, target: &String, dists: &HashMap<String, usize>) -> TargetInfo {
-//     match (
-//         cfg.to_mask.get(target),
-//         cfg.valves.get(target),
-//         dists.get(target),
-//     ) {
-//         (Some(mask), Some(valve), Some(dist)) => TargetInfo {
-//             mask: *mask,
-//             rate: valve.rate,
-//             dist: *dist,
-//         },
-//         _ => todo!(),
-//     }
-// }
-
-// fn process_kid(
-//     cfg: &Config,
-//     cache: &mut Cache,
-//     node: &Node,
-//     target: &String,
-//     dists: &HashMap<String, usize>,
-// ) -> usize {
-//     let info = get_info(cfg, target, dists);
-//     let new_dist = node.dist + info.dist + 1;
-//     let to_end = new_walk(
-//         cfg,
-//         cache,
-//         Node {
-//             name: target.clone(),
-//             seen: node.seen | info.mask,
-//             dist: new_dist,
-//             path: node.path.clone(),
-//         },
-//     );
-
-//     let mut value = new_dist * info.rate;
-
-//     if to_end < usize::MAX {
-//         value += to_end;
-//     }
-
-//     value
-// }
-
-// fn walk(cfg: &Config, node: Node) -> usize {
-//     match cfg.dist_map.get(&node.name) {
-//         Some(dists) => {
-//             let to_visit = get_to_visit(cfg, node.seen);
-
-//             to_visit.iter().fold(usize::MAX, |least, target| {
-//                 match (
-//                     cfg.to_mask.get(*target),
-//                     dists.get(*target),
-//                     cfg.valves.get(*target),
-//                 ) {
-//                     (Some(mask), Some(target_dist), Some(valve)) => {
-//                         let new_seen = node.seen | mask;
-//                         let new_dist = node.dist + target_dist + 1;
-//                         let value = new_dist * valve.rate;
-//                         let mut new_path = node.path.clone();
-
-//                         new_path.push(String::from(*target));
-
-//                         let subtotal = walk(
-//                             cfg,
-//                             Node {
-//                                 name: String::from(*target),
-//                                 seen: new_seen,
-//                                 dist: new_dist,
-//                                 path: new_path,
-//                             },
-//                         );
-
-//                         let mut new_value = value;
-
-//                         if subtotal < usize::MAX {
-//                             new_value += subtotal;
-//                         }
-
-//                         if new_value < least {
-//                             new_value
-//                         } else {
-//                             least
-//                         }
-//                     }
-//                     _ => least,
-//                 }
-//             })
-//         }
-//         None => todo!(),
-//     }
-// }
 
 #[cfg(test)]
 mod tests {
