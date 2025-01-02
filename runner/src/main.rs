5eb0c9fc6b9448ce20073affd8175564d5531680
Rich Heller <rellehr@gmail.com>
2025-01-02 15:58:12 -0600

    Clean up runner code

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,33 +1,24 @@
 use core::AocResult;
 use core::Problem;
 use std::time;
-use year2022::day1;
-use year2022::day10;
-use year2022::day11;
-use year2022::day16;
-use year2022::day2;
-use year2022::day3;
-use year2022::day4;
-use year2022::day5;
-use year2022::day6;
-use year2022::day7;
-use year2022::day8;
-use year2022::day9;
 
-fn problem<T>(f: fn() -> AocResult<T>, expected: T) -> Box<dyn (Fn() -> String)>
+fn problem<T>(label: &'static str, f: fn() -> AocResult<T>, expected: T) -> Problem
 where
     T: 'static + Eq + std::fmt::Debug,
 {
-    Box::new(move || match f() {
-        Ok(actual) => {
-            if actual == expected {
-                String::from("OK")
-            } else {
-                format!("FAILED: expected {:?}, got {:?}", expected, actual)
+    Problem::new(
+        label,
+        Box::new(move || match f() {
+            Ok(actual) => {
+                if actual == expected {
+                    String::from("OK")
+                } else {
+                    format!("FAILED: expected {:?}, got {:?}", expected, actual)
+                }
             }
-        }
-        Err(e) => format!("FAILED: {:?}", e.to_string()),
-    })
+            Err(e) => format!("FAILED: {:?}", e.to_string()),
+        }),
+    )
 }
 
 struct Year {
@@ -39,30 +30,30 @@ fn main() {
     let years: Vec<Year> = vec![Year {
         label: "2022",
         problems: vec![
-            Problem::new("Day1/Part1", problem(day1::part1::solve, 69206)),
-            Problem::new("Day1/Part2", problem(day1::part2::solve, 197400)),
-            Problem::new("Day2/Part1", problem(day2::part1::solve, 11150)),
-            Problem::new("Day2/Part2", problem(day2::part2::solve, 8295)),
-            Problem::new("Day3/Part1", problem(day3::part1::solve, 8039)),
-            Problem::new("Day3/Part2", problem(day3::part2::solve, 2510)),
-            Problem::new("Day4/Part1", problem(day4::part1::solve, 490)),
-            Problem::new("Day4/Part2", problem(day4::part2::solve, 921)),
-            Problem::new("Day5/Part1", problem(day5::part1::solve, String::from("TBVFVDZPN"))),
-            Problem::new("Day5/Part2", problem(day5::part2::solve, String::from("VLCWHTDSZ"))),
-            Problem::new("Day6/Part1", problem(day6::part1::solve, 1707)),
-            Problem::new("Day6/Part2", problem(day6::part2::solve, 3697)),
-            Problem::new("Day7/Part1", problem(day7::part1::solve, 1501149)),
-            Problem::new("Day7/Part2", problem(day7::part2::solve, 10096985)),
-            Problem::new("Day8/Part1", problem(day8::part1::solve, 1543)),
-            Problem::new("Day8/Part2", problem(day8::part2::solve, 595080)),
-            Problem::new("Day9/Part1", problem(day9::part1::solve, 6311)),
-            Problem::new("Day9/Part2", problem(day9::part2::solve, 2482)),
-            Problem::new("Day10/Part1", problem(day10::part1::solve, 15880)),
-            Problem::new("Day10/Part2", problem(day10::part2::solve, "PLGFKAZG")),
-            Problem::new("Day11/Part1", problem(day11::part1::solve, 51075)),
-            Problem::new("Day11/Part2", problem(day11::part2::solve, 11741456163)),
-            Problem::new("Day16/Part1", problem(day16::part1::solve, 1716)),
-            Problem::new("Day16/Part2", problem(day16::part2::solve, 2504)),
+            problem("Day1/Part1", year2022::day1::part1::solve, 69206),
+            problem("Day1/Part2", year2022::day1::part2::solve, 197400),
+            problem("Day2/Part1", year2022::day2::part1::solve, 11150),
+            problem("Day2/Part2", year2022::day2::part2::solve, 8295),
+            problem("Day3/Part1", year2022::day3::part1::solve, 8039),
+            problem("Day3/Part2", year2022::day3::part2::solve, 2510),
+            problem("Day4/Part1", year2022::day4::part1::solve, 490),
+            problem("Day4/Part2", year2022::day4::part2::solve, 921),
+            problem("Day5/Part1", year2022::day5::part1::solve, String::from("TBVFVDZPN")),
+            problem("Day5/Part2", year2022::day5::part2::solve, String::from("VLCWHTDSZ")),
+            problem("Day6/Part1", year2022::day6::part1::solve, 1707),
+            problem("Day6/Part2", year2022::day6::part2::solve, 3697),
+            problem("Day7/Part1", year2022::day7::part1::solve, 1501149),
+            problem("Day7/Part2", year2022::day7::part2::solve, 10096985),
+            problem("Day8/Part1", year2022::day8::part1::solve, 1543),
+            problem("Day8/Part2", year2022::day8::part2::solve, 595080),
+            problem("Day9/Part1", year2022::day9::part1::solve, 6311),
+            problem("Day9/Part2", year2022::day9::part2::solve, 2482),
+            problem("Day10/Part1", year2022::day10::part1::solve, 15880),
+            problem("Day10/Part2", year2022::day10::part2::solve, "PLGFKAZG"),
+            problem("Day11/Part1", year2022::day11::part1::solve, 51075),
+            problem("Day11/Part2", year2022::day11::part2::solve, 11741456163),
+            problem("Day16/Part1", year2022::day16::part1::solve, 1716),
+            problem("Day16/Part2", year2022::day16::part2::solve, 2504),
         ],
     }];
 
